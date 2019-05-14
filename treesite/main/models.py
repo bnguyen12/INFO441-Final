9e5a70559af6cf75716e566fadd70187ef68916a
Benny Nguyen <benny.nguyenx@gmail.com>
2019-05-13 20:09:41 -0700

    Made models

--- treesite/main/models.py
+++ treesite/main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TreeType(models.Model):
+    breed = models.CharField(max_length=250, unique=True)
+    description = models.CharField(max_length=250)
+
+class Trees(models.Model):
+    tree_type_id = models.ForeignKey(TreeType, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    
+    NORMAL = 'NRML'
+    SELLER = 'SLR'
+    ADMIN = 'ADMIN'
+    STATUS_CHOICES = (
+        (SELLER, 'Seller'),
+        (NORMAL, 'Normal'),
+        (ADMIN, 'Admin'),
+    )
+
+    status = models.CharField(
+        max_length = 5,
+        choices = STATUS_CHOICES,
+        default = NORMAL
+    )
+
+class TreeAddress(models.Model):
+    trees_id = models.ForeignKey(Trees, on_delete=models.CASCADE)
+    street = models.CharField(max_length=250)
+    city = models.CharField(max_length=32)
+    zip = models.IntegerField()
+
+class Cart(models.Model):
+    in_cart_id = models.ForeignKey('InCart', on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class InCart(models.Model):
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    trees_id = models.ForeignKey(Trees, on_delete=models.CASCADE)
