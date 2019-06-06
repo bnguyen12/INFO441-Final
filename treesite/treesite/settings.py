da9f6e80c457c87305e67976712206e2148c15c1
Benny Nguyen <benny.nguyenx@gmail.com>
2019-06-05 20:57:42 -0700

    Fix registration form UI

--- treesite/treesite/settings.py
+++ treesite/treesite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
-    'django_extensions'
+    'django_extensions',
+    'materializecssform'
 ]
 
 MIDDLEWARE = [
